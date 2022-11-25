1c80204ad6f02cac5525294a45bb5409ff246da3
manuelJEE <m.escalante.e@hotmail.com>
2022-11-25 17:39:31 -0500

    actualizado CP13

--- CP13/src/index.js
+++ CP13/src/index.js
@@ -1,15 +1,29 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const userRouters=require('./routes/users');
+
 const app = express();
+
 const port = process.env.PORT || 9000;
 
-app.listen(
-    port, ()=>
-    {
-        console.log('server listening on port', port);
-    });
+app.use(express.json());
+app.use('/api', userRouters);
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('conectado a la BD.'))
+.catch((error) => console.log(error));
 
 app.get('/',
 (req, res) => {
 
     res.send("Welcome to my API. !!!");
 });
+
+app.listen(
+    port, ()=>{
+        console.log('server listening on port', port);
+    });
+
+
+
---
+++ CP13/src/routes/users.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const userSchema = require('../models/user');
+
+
+const router = express.Router();
+
+router.post('/users', (req, res) =>{
+
+    const user = userSchema(req.body);
+
+    user
+    .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({message:error}))
+
+
+
+});
+
+
+router.get('/users',(req, res) =>{
+
+    userSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({message:error}))
+
+});
+
+
+router.get('/users/:id',(req, res) =>{
+
+    const {id}=req.params;
+
+    userSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({message:error}))
+
+});
+
+
+
+
+
+module.exports= router;
